a74b0ba4ce6b3baa051f4463098fee783b8b0f9e
Sam Sawchuk <samsawchuk@shaw.ca>
2015-07-29 16:12:06 -0400

    sam initial commit

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,29 @@
 class ProjectsController < ApplicationController
+  
+  def index
+    @projects = Project.all
+  end
+
   def show
+    @project = Project.find(params[:id])
+    @project = @project.build
   end
 
   def new
+    @Project = Project.new
   end
 
   def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to projects_url
+    else
+      render :new
+    end
+  end
+
+  private 
+  def project_params
+    params.require(:project).permit(:name, :city, :description, :initiative_id)
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,7 @@
 class Project < ActiveRecord::Base
+		belongs_to :initiatives
+		has_many tags
+		has_many :interests, through tags
+		has_many :users, through: :interest
+
 end
