a75fbfca1bf38df3bb4ed932e2cc7f7037822638
Craig Franklin <craigmfranklin@gmail.com>
2015-07-30 15:59:59 -0400

    Login and registration completed. User Views completed.

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   # root :to => '#homepagegoeshere'
   resources :user_sessions
   resources :users do
-    resource :interests only, [:new, :create]
+    resource :interests, only: [:new, :create]
   end
 
   get 'login' => 'user_sessions#new', :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
 
-  resource :tags only, [:new, :create]
+  resource :tags, only: [:new, :create]
   resource :projects
   resource :initiatives
 
