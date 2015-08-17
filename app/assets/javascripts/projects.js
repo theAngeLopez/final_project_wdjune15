653b86337d9e0bc88d0e0038b8d170a364b49005
AngeLopez <angel@angelopez.ca>
2015-08-17 18:43:21 -0400

    Further views galore

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -2,14 +2,30 @@ $(document).on('ready', function() {
   $('#search-form').on('submit', function(event) {
     event.preventDefault();
 
+    // debugger
     var searchValue = $('#search-form input:checked').map(function(value, key) { return key.value });
+    // console.log(searchValue);
+    // debugger
     var searchValueParams = $.param({tags: $.makeArray(searchValue)});
+    // console.log(searchValueParams);
+    // debugger
 
     $.ajax({
       url: '/projects?' + searchValueParams,
       method: 'GET',
       dataType: 'script'
     });
+
+    // var params = { tags: $('#search-form input:checked').serialize;
+
+    // params =
+
+    // $.ajax({
+    //   url: '/projects?',
+    //   method: 'GET',
+    //   dataType: 'script',
+    //   data: { tags: params }
+    // });
 
   });
 });
